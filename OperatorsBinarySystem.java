2b20222229babbfa9297e86648d7e3c56ce8eb3c
HARISH KUMAR GUPTA <88849351+Harish2003k@users.noreply.github.com>
2022-05-06 06:32:15 +0530

    Add files via upload
    commit

---
+++ OperatorsBinarySystem.java
@@ -0,0 +1,32 @@
+public class OperatorsBinarySystem {
+	public static void main(String args[]) {
+	/*	int a=10; this is arthimetric operation in java
+		int b=3;
+		a=a+b;
+		System.out.println(a);
+		a=a-b;
+		System.out.println(a);
+		a=a*b;
+		System.out.println(a);
+		a=a/b;
+		System.out.println(a);
+		a=a%b;
+		System.out.println(a);
+	*/
+		int a=10;   // unary operator in java post increemnent 
+		int b=0;
+		b=a++;
+		System.out.println(a);
+		System.out.println(b);
+		b=++a;
+		System.out.println(a);
+		System.out.println(b);
+		b=a--;
+		System.out.println(a);
+		System.out.println(b);
+		b=--a;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
